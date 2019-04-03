d12bec3169d3a52f318019f1540867daa356e8f8
jordyboer0404 <jordy.boer.040400@gmail.com>
2019-04-03 13:20:57 +0200

    home pagina

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NieuwsRepository;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(NieuwsRepository $nieuwsRepository)
     {
+        $user = $this->getUser();
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'user' => $user,
+            'nieuws' => $nieuwsRepository->findBy([], ['datum' => 'DESC'])
         ]);
     }
 }
